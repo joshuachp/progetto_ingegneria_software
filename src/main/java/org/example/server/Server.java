c3501e783b0720a94480f7ed2a9c86edcd04ba73
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-05-20 21:58:58 +0200

    Added BCrypt

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,5 +1,8 @@
 package org.example.server;
 
+
+import org.hibernate.jpa.HibernateQuery;
+
 public class Server {
 
     // Signleton del server
@@ -9,6 +12,19 @@ public class Server {
 
     // ENDPOINTS:
     // - autenticazione
+
+    /**
+     * Autentica un utente. Controlla la password con l'hash della password nel database, se coincidono ritorna una
+     * token sessione utente.
+     *
+     * @param username Username dell'utente
+     * @param password Password dell'utente
+     * @return TODO: Define the return data
+     */
+    boolean autenticateUser(String username, String password) {
+        HibernateQuery()
+        return BCrypt.checkpw(password, hash);
+    }
 
     // - deautenticazione
 }
---
+++ src/main/java/org/example/server/models/User.java
@@ -0,0 +1,37 @@
+package org.example.server.models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    // TODO: Session handling
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
