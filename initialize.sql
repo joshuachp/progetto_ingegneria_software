eae19cfac7394e3cd03fbd74fd6640353840058b
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-05-21 10:53:59 +0200

    Added bcrypt password to sql script

--- initialize.sql
+++ initialize.sql
@@ -4,4 +4,4 @@ CREATE TABLE users (
   password TEXT NOT NULL
 );
 INSERT INTO users (username, password)
-VALUES('admin', 'password');
+VALUES('admin', '$2y$10$WRZeOh.bOOboXuxL1s2aAO8Fos5SUodtHq77IdE8Kp.3bunaFe.dy');
