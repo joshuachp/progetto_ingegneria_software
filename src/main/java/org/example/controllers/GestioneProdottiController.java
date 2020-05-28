a6f35ac697177519583ae16e6243081b97d4dd1d
Xidreal <45039373+xidreal@users.noreply.github.com>
2020-05-28 21:55:35 +0200

    added temporary redirect to Responsabile reparto

---
+++ src/main/java/org/example/controllers/GestioneProdottiController.java
@@ -0,0 +1,18 @@
+package org.example.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GestioneProdottiController {
+    public void showView(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/gestioneProdotti.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Gestione Prodotti");
+        stage.show();
+    }
+}
--- src/main/java/org/example/models/ResponsabileReparto.java
+++ src/main/java/org/example/models/ResponsabileReparto.java
@@ -1,14 +1,24 @@
 package org.example.models;
 
 import javafx.stage.Stage;
+import org.example.controllers.CatalogoController;
+import org.example.controllers.GestioneProdottiController;
+
+import java.io.IOException;
 
 public class ResponsabileReparto extends Utente {
     public ResponsabileReparto(String username, String session) {
         super(username, session, true);
     }
 
+    // TODO: temporary choice, only to check the function
     @Override
     public void redirect(Stage stage) {
-        // TODO
+        GestioneProdottiController gestioneProdotti = new GestioneProdottiController();
+        try {
+            gestioneProdotti.showView(stage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
