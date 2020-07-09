a00c23efaafdc6e7f47dae166e505f284892562b
Xidreal <45039373+xidreal@users.noreply.github.com>
2020-07-09 15:32:59 +0200

    Update Utils.java

--- src/main/java/org/example/client/utils/Utils.java
+++ src/main/java/org/example/client/utils/Utils.java
@@ -3,11 +3,7 @@ package org.example.client.utils;
 import javafx.stage.Stage;
 import okhttp3.*;
 import org.example.client.controllers.AuthController;
-import org.example.client.models.Client;
-import org.example.client.models.Manager;
-import org.example.client.models.Order;
-import org.example.client.models.User;
-import org.example.client.models.enums.Payment;
+import org.example.client.models.*;
 import org.intellij.lang.annotations.RegExp;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -15,10 +11,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -237,23 +231,41 @@ public class Utils {
         return null;
     }
 
-    public static @Nullable Response createOrder(Payment payment, Date deliveryStart, Date deliveryEnd, int state,
-                                                 int userID) {
-        /*OkHttpClient client = new OkHttpClient();
-        RequestBody body = new FormBody.Builder()
-                .add("session", session)
-                .build();
+
+    /**
+     * Create Order.
+     *
+     * @param session
+     * @param products
+     * @param deliveryStart
+     * @param deliveryEnd
+     * @throws Exception
+     */
+    public static void createOrder(String session, List<Product> products, Date deliveryStart, Date deliveryEnd) throws Exception {
+
+        Map<Integer, Integer> productMap = products.stream().collect(Collectors.toMap(Product::getId,
+                Product::getQuantity));
+
+        JSONObject json = new JSONObject()
+                .put("session", session)
+                .put("products", productMap)
+                .put("deliveryStart", deliveryStart.getTime())
+                .put("deliveryEnd", deliveryEnd.getTime());
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(json.toString(), MediaType.get("application/json; charset=utf-8"));
+
         Request request = new Request.Builder()
-                .url(String.format(SERVER_URL + SERVER_URL_CREATE_ORDER))
+                .url(SERVER_URL + SERVER_URL_GET_ALL_ORDERS)
                 .post(body)
                 .build();
-        try {
-            return client.newCall(request).execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        return null;
+        Response response = client.newCall(request).execute();
+        if (response.code() != 200) {
+            String error = Objects.requireNonNull(response.body()).string();
+            Objects.requireNonNull(response.body()).close();
+            throw new Exception(error);
 
+        }
     }
 
     /**
@@ -287,7 +299,8 @@ public class Utils {
      *
      * @param session User session
      * @return List of the user orders
-     * @throws Exception {@link IOException} if request fails and {@link Exception} if the requests returns error
+     * @throws Exception {@link IOException} if request fails and {@link Exception} if the requests returns
+     *                   error
      *                   code. Sets the request body as the exception message
      */
     public static @NotNull List<Order> getAllOrders(String session) throws Exception {
@@ -296,7 +309,7 @@ public class Utils {
                 .add("session", session)
                 .build();
         Request request = new Request.Builder()
-                .url(SERVER_URL + SERVER_URL_GET_ALL_ORDERS)
+                .url(SERVER_URL + SERVER_URL_CREATE_ORDER)
                 .post(body)
                 .build();
         Response response = client.newCall(request).execute();
